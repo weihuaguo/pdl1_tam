cf33afc8fa5608900d1d88e0901d8ad154980809
weihuaguo <gwh120104@gmail.com>
2023-10-06 15:37:46 -0700

    Add the TAM subcluster and resolution screening and cluster tree

--- scrna_in_house.R
+++ scrna_in_house.R
@@ -32,7 +32,7 @@ srQCPrePro <- function(srsc, plotPf, res = 300) {
 
 inteSrPro <- function(srsc, plotPf, batchName = NULL, res = 300, ndim = 18, rdsSave = FALSE, plotFeat = NULL, jsFlag = FALSE) {
 	qcgg <- VlnPlot(srsc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, group.by = batchName)
-	ggsave(plot = qcgg, filename = paste(plotPf, "QCPlot1.png", sep = ""), dpi = res, width = 9, height = 6) # FigS1x
+	ggsave(plot = qcgg, filename = paste(plotPf, "QCPlot1.png", sep = ""), dpi = res, width = 9, height = 6) 
 
 	srsc <- ScaleData(srsc)
 
@@ -56,13 +56,13 @@ inteSrPro <- function(srsc, plotPf, batchName = NULL, res = 300, ndim = 18, rdsS
 		srsc <- JackStraw(srsc, num.replicate = 100)
 		srsc <- ScoreJackStraw(srsc, dims = 1:20)
 		jsGG <- JackStrawPlot(srsc, dims = 1:20)
-		ggsave(plot = jsGG, filename = paste(plotPf, "JackStraw.png", sep = ""), dpi = res, width = 9, heigh = 6) #FigS1x
+		ggsave(plot = jsGG, filename = paste(plotPf, "JackStraw.png", sep = ""), dpi = res, width = 9, heigh = 6)
 		cat("Jack Straw costs")
 		print(Sys.time()-jst)
 	}
 
 	elGG <- ElbowPlot(srsc, ndims = 50)
-	ggsave(plot = elGG, filename = paste(plotPf, "Elbow.png", sep = ""), dpi = res, width = 9, heigh = 6) #FigS1x
+	ggsave(plot = elGG, filename = paste(plotPf, "Elbow.png", sep = ""), dpi = res, width = 9, heigh = 6)
 
 	srsc <- FindNeighbors(srsc, dims = 1:ndim)
 	srsc <- FindClusters(srsc)
@@ -74,23 +74,23 @@ inteSrPro <- function(srsc, plotPf, batchName = NULL, res = 300, ndim = 18, rdsS
 
 	if (!is.null(plotFeat)) {
 		cat("Start to plot feature plots and violin plots\n")
-		vlnGG <- VlnPlot(srsc, features = plotFeat, slot = "counts", log = TRUE, ncol = 3, pt.size = 0.5)
+		vlnGG <- VlnPlot(srsc, features = plotFeat, slot = "data", log = TRUE, ncol = 3, pt.size = 0.5)
 		ggsave(plot = vlnGG, filename = paste(plotPf, "violin_plot_for_cell_annotation.png", sep = ""), 
-		       dpi = res, width = 16, height = 30) # FigS1x
+		       dpi = res, width = 16, height = 30) 
 
 		featGG <- FeaturePlot(srsc, features = plotFeat, ncol = 3, pt.size = 0.2, sort.cell = TRUE)
 		ggsave(plot = featGG, filename = paste(plotPf, "umap_feature_plot_for_cell_annotation.png", sep = ""), 
-		       dpi = res, width = 16, height = 30) #FigS1x
+		       dpi = res, width = 16, height = 30)
 
 		dotGG <- DotPlot(srsc, features = plotFeat) + RotatedAxis()
 		ggsave(plot = dotGG, filename = paste(plotPf, "DotPlot_for_cell_annotation.png", sep = ""),
-		       dpi = res, width = 9, height = 6) #FigS1x
+		       dpi = res, width = 9, height = 6)
 	}
 
 	if (!is.null(batchName)) {
 		batchCheckGG <- DimPlot(srsc, reduction = "umap", group.by = batchName)
 		ggsave(plot = batchCheckGG, filename = paste(plotPf, "batch_effect_check_dimPlot.png", sep = ""),
-		       dpi = res, width = 9, height = 6) #FigS1x
+		       dpi = res, width = 9, height = 6)
 	}
 	
 	srsc.markers <- FindAllMarkers(srsc, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
@@ -100,15 +100,11 @@ inteSrPro <- function(srsc, plotPf, batchName = NULL, res = 300, ndim = 18, rdsS
 	write.csv(topMarkers, file = paste(plotPf, "top10_pos_markers.csv", sep = "")) 
 
 	topMarkers <- srsc.markers %>% group_by(cluster) %>% top_n(n = 5, wt = avg_log2FC)
-#	png(paste(plotPf, "top5_marker_heatmap.png", sep = ""), res = res, width = 16, height = 18, units = "in")
-#	print(DoHeatmap(object = srsc, features = topMarkers$gene) + NoLegend())
-#	gar = dev.off()
 	hm <- DoHeatmap(object = srsc, features = topMarkers$gene) + NoLegend()
 	ggsave(paste(plotPf, "top5_marker_heatmap.png", sep = ""), hm, dpi = res, width = 24, height = 18)
 
 	topDotMarkers <- srsc.markers %>% group_by(cluster) %>% top_n(n = 5, wt = avg_log2FC)
 	geneMarkers <- unique(as.character(topDotMarkers$gene))
-#	geneMarkers = unique(c(as.character(topDotMarkers$gene), "CD14", "CD68", "HLA-DRA", "CD163", "STAB1"))
 
 	dotMarkerGG <- DotPlot(srsc, features = geneMarkers) + RotatedAxis() + coord_flip()
 	ggsave(plot = dotMarkerGG, filename = paste(plotPf, "Top5Marker_DotPlot.png", sep = ""), 
@@ -119,6 +115,85 @@ inteSrPro <- function(srsc, plotPf, batchName = NULL, res = 300, ndim = 18, rdsS
 		saveRDS(srsc, file = paste(plotPf, "Seurat_Objects_Clustered.RDS", sep = ""))
 	}
 	print(srsc)
+	return(srsc)
+}
+
+resScreen <- function(srsc, plotPf, plotDir, batchName = NULL, res = 300, ndim = 18, rdsSave = FALSE, plotFeat = NULL, jsFlag = FALSE, resolutions) {
+	qcgg <- VlnPlot(srsc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, group.by = batchName)
+	ggsave(plot = qcgg, filename = paste(plotPf, "QCPlot1.png", sep = ""), dpi = res, width = 9, height = 6) 
+
+	srsc <- ScaleData(srsc)
+
+	#############
+	srsc <- RunPCA(srsc)
+
+	elGG <- ElbowPlot(srsc, ndims = 50)
+	ggsave(plot = elGG, filename = paste(plotPf, "Elbow.png", sep = ""), dpi = res, width = 9, heigh = 6)
+	srsc <- FindNeighbors(srsc, dims = 1:ndim)
+	srsc <- RunUMAP(srsc, dims = 1:ndim)
+
+	if (!is.null(batchName)) {
+		batchCheckGG <- DimPlot(srsc, reduction = "umap", group.by = batchName)
+		ggsave(plot = batchCheckGG, filename = paste(plotPf, "batch_effect_check_dimPlot.png", sep = ""),
+		       dpi = res, width = 9, height = 6)
+	}
+
+
+	for (ires in resolutions) {
+		resFolder <- paste("tam_subcluster_res", ires, sep = "_")
+		dir.create(file.path(plotDir, resFolder), showWarnings = FALSE)
+		resDir <- paste(plotDir, resFolder, sep = "/")
+		rppf <- paste(resDir, "/", resFolder, "_", sep = "")
+
+		srsc <- FindClusters(srsc, resolution = ires)
+
+		srsc <- BuildClusterTree(srsc)
+		png(paste(rppf, "cluster_tree.png", sep = ""), res = 300, width = 12, height = 18, unit = 'in')
+		PlotClusterTree(object = srsc)
+		gar <- dev.off()
+
+		umapGG <- DimPlot(srsc, reduction = "umap", label = TRUE)
+		ggsave(plot = umapGG, filename = paste(rppf, "UMAP.png", sep = ""), dpi = res, width = 9, heigh = 6)
+
+		if (!is.null(plotFeat)) {
+			cat("Start to plot feature plots and violin plots\n")
+			vlnGG <- VlnPlot(srsc, features = plotFeat, slot = "counts", log = TRUE, ncol = 3, pt.size = 0.5)
+			ggsave(plot = vlnGG, filename = paste(rppf, "violin_plot_for_cell_annotation.png", sep = ""), 
+			       dpi = res, width = 16, height = 16) 
+
+			featGG <- FeaturePlot(srsc, features = plotFeat, ncol = 3, pt.size = 0.2, sort.cell = TRUE)
+			ggsave(plot = featGG, filename = paste(rppf, "umap_feature_plot_for_cell_annotation.png", sep = ""), 
+			       dpi = res, width = 16, height = 16)
+
+			dotGG <- DotPlot(srsc, features = plotFeat) + RotatedAxis()
+			ggsave(plot = dotGG, filename = paste(rppf, "DotPlot_for_cell_annotation.png", sep = ""),
+			       dpi = res, width = 9, height = 6)
+		}
+
+		
+		srsc.markers <- FindAllMarkers(srsc, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+		topMarkers <- srsc.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_log2FC)
+
+		write.csv(srsc.markers, file = paste(rppf, "ALL_POS_MARKERS.csv", sep = "")) #ST1x
+		write.csv(topMarkers, file = paste(rppf, "top10_pos_markers.csv", sep = "")) 
+
+		topMarkers <- srsc.markers %>% group_by(cluster) %>% top_n(n = 5, wt = avg_log2FC)
+		hm <- DoHeatmap(object = srsc, features = topMarkers$gene) + NoLegend()
+		ggsave(paste(rppf, "top5_marker_heatmap.png", sep = ""), hm, dpi = res, width = 24, height = 18)
+
+		topDotMarkers <- srsc.markers %>% group_by(cluster) %>% top_n(n = 5, wt = avg_log2FC)
+		geneMarkers <- unique(as.character(topDotMarkers$gene))
+
+		dotMarkerGG <- DotPlot(srsc, features = geneMarkers) + RotatedAxis() + coord_flip()
+		ggsave(plot = dotMarkerGG, filename = paste(rppf, "Top5Marker_DotPlot.png", sep = ""), 
+		       dpi = res, width = 9, height = 16)
+
+		
+		if (rdsSave) {
+			saveRDS(srsc, file = paste(rppf, "Seurat_Objects_Clustered.RDS", sep = ""))
+		}
+		print(srsc)
+	}
 	return(srsc)
 }
 
@@ -133,6 +208,8 @@ suppressMessages(library(stringr))
 suppressMessages(library(readxl))
 suppressMessages(library(RColorBrewer))
 suppressMessages(library(MAST))
+suppressMessages(library(clustree))
+
 
 workDir <- "/home/weihua/mnts/smb_plee/Group/weihua/pdl1_data_hub"
 dataDir <- paste(workDir, "1_inhouse_scrna_seq", sep = "/") # Please change this to repeat the work
@@ -143,7 +220,8 @@ readFlag <- FALSE
 inteFlag <- FALSE
 inteFilter <- 100 # Minimum cell number for integration
 clstFlag <- FALSE
-majorVisFlag <- TRUE
+majorVisFlag <- FALSE
+tamClusterFlag <- TRUE
 
 dir.create(file.path(resDir, expID), showWarnings = FALSE)
 expDir <- paste(resDir, expID, sep = "/")
@@ -407,6 +485,55 @@ if (majorVisFlag) {
 
 	ggsave(plot = figs1f, filename = paste(intePf, "FigS1F_UMAP.png", sep = ""), dpi = 600, width = 9, heigh = 6)
 	saveRDS(figs1f, paste(intePf, "FigS1F.RDS", sep = ""))
+
+}
+
+if (tamClusterFlag) {
+	cat("Cluster TAMs...\n")
+	proObj <- readRDS(paste(intePf, "Seurat_Objects_Clustered.RDS", sep = ""))
+
+	ist <- Sys.time()
+	subSrsc <- subset(proObj, idents = c(4,15,16)) # NOTE: ALL_POS_MARKER.xlsx
+
+	newSrsc <- CreateSeuratObject(counts = subSrsc[["RNA"]]@counts, meta.data = subSrsc@meta.data)
+	seuratList <- SplitObject(newSrsc, split.by = "patient")
+	
+	subFolder <- "tam_sub_cluster"
+	dir.create(file.path(expDir, subFolder), showWarnings = FALSE)
+	subDir <- paste(expDir, subFolder, sep = "/")
+	subPrefix <- paste(subDir, "/", expID, "_", subFolder, "_", sep = "")
+
+	for (i in names(seuratList)) {
+		cat("Patient", i, ":", length(colnames(seuratList[[i]])), "\n")
+		seuratList[[i]] <- NormalizeData(seuratList[[i]], verbose = FALSE)
+		seuratList[[i]] <- FindVariableFeatures(seuratList[[i]], selection.method = "vst", nfeatures = 2000, verbose = FALSE)
+		if (length(colnames(seuratList[[i]])) <= 50) {
+			## NOTE: remove the patients with few cells (<100 cells) for integration!
+			seuratList[[i]] <- NULL
+			cat("\tPatient", i, "is removed due to small cell number!!!\n")
+		}
+	}
+	integrateAnchors <- FindIntegrationAnchors(object.list = seuratList, k.filter = 50)
+	inteObjs <- IntegrateData(anchorset = integrateAnchors, k.weight = 50)
+	DefaultAssay(inteObjs) <- "integrated"
+	saveRDS(inteObjs, paste(subPrefix, "_Integrated_results.RDS", sep = ""))
+	cat("Integration time cost:")
+	print(Sys.time()-ist)
+
+	pst <- Sys.time()
+	ress <- c(seq(0.1,0.9, 0.1), seq(1.0,2.0,0.2))
+	geneOi <- c("CD14", "CD68", "HLA-DRA", "CD274", "SIGLEC15", "PDCD1LG2", "CD1C", "ITGAX", "FCER1A", "TPSAB1", "KIT", "FUT4")
+	inteProObj <- resScreen(inteObjs, plotPf = subPrefix, plotDir = subDir, rdsSave = TRUE, jsFlag = FALSE, ndim = 20,
+				batchName = c("patient"), plotFeat = geneOi, resolutions = ress)
+	cat("Analysis time cost:")
+	print(Sys.time()-pst)
+
+	all_cols <- brewer.pal(length(ress), 'Set1')
+	colnames(inteProObj@meta.data)[colnames(inteProObj@meta.data) == "integrated_snn_res.1"] <- "integrated_snn_res.0.0"
+	cltr <- clustree(inteProObj, prefix = "integrated_snn_res.") + scale_color_manual(values = all_cols)
+	ggsave(paste(subPrefix, "clustree.png", sep = ""), cltr, dpi = 300, width = 12, height = 36)
+
+	cat("++++++++++++++++++++++++++++++++++++++++++++++++\n\n")
 
 }
 # TODO: Subcluster TAM/TAM+DC, check the PD-L1+ and SIGLEC15+ cell number, screen the resolution
